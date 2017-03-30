61921b9c921fe825c8ebb9ab635c12f586aa562a
Meteodeep <dobg1.91@gmail.com>
2017-03-30 16:51:46 +0100

    Writing Text Example

---
+++ TextWriteExample.py
@@ -0,0 +1,9 @@
+save=["Climate Change in your Life:"]
+    save.append(result)
+    print (save)
+ 
+    target1 = open('yourlocalclimate.txt', 'w')
+ 
+    target1.write(result)
+ 
+    target1.close()
