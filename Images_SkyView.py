8141d355ac8c9dea37de890f12ab1a20f049ad7b
Meteodeep <dobg1.91@gmail.com>
2017-06-13 15:25:07 +0100

    Create Images_SkyView.py

---
+++ Images_SkyView.py
@@ -0,0 +1,12 @@
+from picamera import PiCamera
+from time import sleep
+
+camera = PiCamera()
+
+camera.start_preview()
+sleep(5)
+camera.capture('/home/pi/Desktop/image.jpg')
+camera.stop_preview()
+
+#add crontab
+#add Dropbox-Uploader
