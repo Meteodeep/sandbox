1a2aa8de8fa3526cc2fd8ae56dfa10d80ccdbe73
Meteodeep <dobg1.91@gmail.com>
2017-08-12 18:54:22 +0100

    Add files via upload

---
+++ Summer-2017/Thereoms.py
@@ -0,0 +1,25 @@
+import math
+
+while True:
+
+    print("Thereom 13")
+
+    print("'y1' and 'y2' are Triangle1 and 'x1' and 'x2' are Triangle2")
+    print("'x1' is the Variable, set as '0'")
+
+    y1=(int(input("Please enter Y1")))
+    y2=(int(input("Please enter Y2")))
+    x1=(int(input("Please enter X1")))
+    x2=(int(input("Please enter X2")))
+
+
+    ans1 = y1*x2
+    ans2 = ans1/y2
+
+    print(ans2)
+        
+        
+        
+
+
+
