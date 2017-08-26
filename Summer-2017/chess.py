81e00d34555070ce8ca53115293278f028eb85d3
Meteodeep <dobg1.91@gmail.com>
2017-08-26 22:50:53 +0100

    Create chess.py

---
+++ Summer-2017/chess.py
@@ -0,0 +1,12 @@
+rom time import sleep
+from random import randint
+import random
+import string
+import math
+
+while True:
+
+        print(random.choice(string.ascii_letters[0:7]))
+        print(randint(1,8))
+        #print("%s, %d" % ((random.choice(string.ascii_letters[0:7]), (randint(1,8))))
+        sleep(10)
