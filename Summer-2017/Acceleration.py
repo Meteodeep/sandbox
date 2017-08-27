552cac5478659b164bbe3ee6a29be99c1f1b0473
Meteodeep <dobg1.91@gmail.com>
2017-08-27 16:04:56 +0100

    Update Acceleration.py

--- Summer-2017/Acceleration.py
+++ Summer-2017/Acceleration.py
@@ -1,6 +1,6 @@
-velocitystart=(int(input("Please enter the Velocity at the Start (in m/s^2)")))
-velocityfinish=(int(input("Please enter the Velocity at the Finish (in m/s^2)")))
-time=(int(input("Please enter the Time (in Seconds)")))
+velocitystart=(float(input("Please enter the Velocity at the Start (in m/s^2)")))
+velocityfinish=(float(input("Please enter the Velocity at the Finish (in m/s^2)")))
+time=(float(input("Please enter the Time (in Seconds)")))
 
 velocitychange = velocityfinish-velocitystart
 acceleration = velocitychange/time
