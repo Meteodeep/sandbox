65d6b48c0fda7b69fd0389540c9f28479a106fcf
Meteodeep <dobg1.91@gmail.com>
2017-08-27 11:23:25 +0100

    Update Chess.py

--- Summer-2017/Chess.py
+++ Summer-2017/Chess.py
@@ -1,12 +1,11 @@
 rom time import sleep
 from random import randint
-import random
-import string
+import random #numbers
+import string #letters
 import math
 
 while True:
 
-        print(random.choice(string.ascii_letters[0:7]))
-        print(randint(1,8))
-        #print("%s, %d" % ((random.choice(string.ascii_letters[0:7]), (randint(1,8))))
+        print(random.choice(string.ascii_letters[0:7])) #splicing the Alphabet
+        print(randint(1,8)) #Numbers: 1-8 Range
         sleep(10)
