ab4057b8d84066da7b45f7988e35cf516be5dde5
Meteodeep <dobg1.91@gmail.com>
2017-08-27 11:14:43 +0100

    Update co2.py

--- Summer-2017/co2.py
+++ Summer-2017/co2.py
@@ -10,7 +10,7 @@ while True:
 
         currentCO2 = currentCO2 + currentyearlyincrease + yearbyyearincrease
         print(currentCO2)
-        yearbyyearincrease = yearbyyearincrease + 2
+        yearbyyearincrease = yearbyyearincrease * 2
         sleep(5)
 
 
