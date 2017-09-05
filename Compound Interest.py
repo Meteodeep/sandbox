b4d3f87930a3362296e30ceb56be139d1b61ca6d
Meteodeep <dobg1.91@gmail.com>
2017-09-05 17:14:19 +0100

    Add files via upload

---
+++ Compound Interest.py
@@ -0,0 +1,8 @@
+p=(float(input("Please enter original investment")))
+i=(float(input("Please enter the Intrest Rate (in Decimal Places)")))
+
+f = p*i
+
+n = p + f
+
+print(n)
---
+++ Simple Interest.py
@@ -0,0 +1,9 @@
+p=(float(input("Please enter the Starting Investment")))
+i=(float(input("Please enter the Interest Rate (in Decimal Places)")))
+t=(float(input("Please enter the Time (in Years)")))
+
+fullvalue=(float(fullvalue))
+
+p*1+i**3 = fullvalue
+
+print(fullvalue)
