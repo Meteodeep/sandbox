88acbc41e8cff5b7fb010a0fa79183e71c56a1ad
Meteodeep <dobg1.91@gmail.com>
2017-09-15 22:54:46 +0100

    Update Moment.py

--- Summer-2017/Moment.py
+++ Summer-2017/Moment.py
@@ -1,7 +1,7 @@
 force=(float(input("Please enter the Force applied (in Newtons)")))
-perpindicular=(float(input("Please enter the Perpindicualr Distance from the Force to the Fulcrum")))
+perpindicular=(float(input("Please enter the Perpindicualr Distance from the Force to the Fulcrum(In Metres)")))
 
 moment = force*perpindicular
 moment = (str(moment))
 
-print(moment + "N")
+print(moment + "Nm")
