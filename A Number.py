62575c47eb0658326a2c2655c64c610f9955a172
Meteodeep <dobg1.91@gmail.com>
2017-10-07 11:49:12 +0100

    Update A Number.py

--- A Number.py
+++ A Number.py
@@ -10,15 +10,15 @@ while(count < 5):
     
     mynumber=(int(input("Guess the Number?")))
 
-    if mynumber < numberone:
+    if mynumber < number:
         print("HIGHER")
         count = count + 1
 
-    elif mynumber > numberone:
+    elif mynumber > number:
         print("LOWER")
         count = count + 1
 
-    elif mynumber == numberone:
+    elif mynumber == number:
         print("WELL DONE!")
 
 
