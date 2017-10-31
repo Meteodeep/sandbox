4f4a4419709910c742f7720414c73ac5b912aa9d
Meteodeep <dobg1.91@gmail.com>
2017-10-31 10:38:42 +0000

    Shake

---
+++ Shake.py
@@ -0,0 +1,20 @@
+from sense_hat import SenseHat
+
+sense = SenseHat()
+
+red = (255, 0, 0)
+
+while True:
+    acceleration = sense.get_accelerometer_raw()
+    x = acceleration['x']
+        y = acceleration['y']
+        z = acceleration['z']
+
+    x = abs(x)
+    y = abs(y)
+    z = abs(z)
+
+    if x > 1 or y > 1 or z > 1:
+        sense.show_letter("!", red)
+    else:
+        sense.clear()
