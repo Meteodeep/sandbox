12de25b26d35a37ca01099de714e3ad97a5cda55
Meteodeep <dobg1.91@gmail.com>
2017-11-08 10:47:41 +0000

    Create Dict.py

---
+++ Dict.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+dict = {'Name': 'Zara', 'Age': 7, 'Class': 'First'}
+del dict['Name']; # remove entry with key 'Name'
+dict.clear();     # remove all entries in dict
+del dict ;        # delete entire dictionary
+
+print "dict['Age']: ", dict['Age']
+print "dict['School']: ", dict['School']
