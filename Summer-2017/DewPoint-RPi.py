74b2542d51f1058a2b4ba2e841054acebc8936cb
Meteodeep <dobg1.91@gmail.com>
2017-12-21 16:07:29 +0000

    Create DewPoint-RPi.py

---
+++ Summer-2017/DewPoint-RPi.py
@@ -0,0 +1,21 @@
+from sense_hat import SenseHat
+
+sense = SenseHat()
+sense.clear()
+
+temp = sense.get_temperature()
+
+humidity = sense.get_humidity()
+
+humidity = round(humidity, 1)
+
+calc = 100-humidity
+
+calc2 = calc/5
+
+dewpoint = temp - calc2
+
+print(dewpoint)
+
+sleep(25)
+
