2d68aa4e0467032ff23718d19b929a00218d5718
Meteodeep <dobg1.91@gmail.com>
2018-01-12 13:14:47 +0000

    SQL

---
+++ Sql.sql
@@ -0,0 +1,6 @@
+CREATE TABLE coding (id INTEGER PRIMARY KEY, name TEXT, quantity INTEGER );
+
+INSERT INTO coding VALUES (1, "Raspberry Pi", 4);
+INSERT INTO coding VALUES (2, "Arduino", 1);
+INSERT INTO coding VALUES (3, "Micro:Bit", 2);
+SELECT * FROM coding;
