34e79d49669af5ab2300ff1676d2641016e3db0f
Meteodeep <dobg1.91@gmail.com>
2018-01-17 22:13:41 +0000

    Python 3

---
+++ Python.py
@@ -0,0 +1,24 @@
+import string
+import math
+import webbrowser
+import random
+import time
+from time import sleep
+
+def square(x,y): #functions for a square
+    return x*y
+
+time.sleep(3)
+
+list(string.ascii_lowercase)
+
+print (string.ascii_lowercase[12:18])
+
+print(random.choice(string.ascii_lowercase))
+
+alphabetletters = string.ascii_lowercase
+
+webbrowser.open_new_tab("google.com")
+
+
+square(3,3)
