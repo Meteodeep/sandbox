0e2815cb4ceaf91b5be9c57466fc922a0692fd43
Meteodeep <dobg1.91@gmail.com>
2018-01-26 22:20:02 +0000

    Update TurtleDraw.py

--- TurtleDraw.py
+++ TurtleDraw.py
@@ -1,13 +1,16 @@
 #Edit function to change shape
+import random
 from turtle import *
 canvas = Screen()
+
+number = random.randrange(0, 800)
 
 
 turtle = Turtle()
 
 for i in range(4):
-    turtle.forward(100)
-    turtle.left(90)
+    turtle.forward(100) #number
+    turtle.left(90)  #number
 
 canvas.exitonclick()
 
