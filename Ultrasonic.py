8e477d2825f731ca822d79ad46d24869c7e31ead
Meteodeep <dobg1.91@gmail.com>
2018-02-03 10:31:12 +0000

    Create Ultrasonic.py

---
+++ Ultrasonic.py
@@ -0,0 +1,37 @@
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
+
+TRIG = 23 
+ECHO = 24
+
+print "Distance Measurement In Progress"
+
+GPIO.setup(TRIG,GPIO.OUT)
+GPIO.setup(ECHO,GPIO.IN)
+
+GPIO.output(TRIG, False)
+print "Waiting For Sensor To Settle"
+time.sleep(2)
+
+
+def distcheck():
+
+  GPIO.output(TRIG, True)  #fires a pulse
+  time.sleep(0.00001)
+  GPIO.output(TRIG, False)
+
+  while GPIO.input(ECHO)==0:  #measure some times
+    pulse_start = time.time()
+
+  while GPIO.input(ECHO)==1:
+    pulse_end = time.time()
+
+  pulse_duration = pulse_end - pulse_start  #does the maths
+
+  distance = pulse_duration * 17150
+
+  distance = round(distance, 2)
+
+  print "Distance:",distance,"cm"
+  return distance
