7641b62245ec4fa91ac55d11d08b6a99ebdd6b24
Meteodeep <dobg1.91@gmail.com>
2018-02-23 12:52:29 +0000

    Update Dict.py

--- Dict.py
+++ Dict.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python
 
-dict = {'Name': 'Zara', 'Age': 7, 'Class': 'First'}
+dict = {'Name': 'John', 'Age': 7, 'Class': 'First'}
 del dict['Name']; # remove entry with key 'Name'
 dict.clear();     # remove all entries in dict
 del dict ;        # delete entire dictionary
+dict['Surname'] = "Appleseed"
 
 print "dict['Age']: ", dict['Age']
 print "dict['School']: ", dict['School']
