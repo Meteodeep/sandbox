aedd9d167352f42fdb07463111d0a38059968280
Meteodeep <dobg1.91@gmail.com>
2018-02-28 20:08:25 +0000

    Yes!

---
+++ GetWeatherAPI.py
@@ -0,0 +1,81 @@
+import datetime
+import json
+import urllib.request
+from time import sleep
+import webbrowser
+import math
+
+
+def time_converter(time):
+    converted_time = datetime.datetime.fromtimestamp(
+        int(time)
+    ).strftime('%I:%M %p')
+    return converted_time
+
+
+def url_builder(city_id):
+    user_api = ''  #add your API key here!
+    unit = 'metric'  
+    api = 'http://api.openweathermap.org/data/2.5/weather?q=Dublin,ie'
+
+    full_api_url = api + str(city_id) + '&mode=json&units=' + unit + '&APPID=' + user_api
+    return full_api_url
+
+
+def data_fetch(full_api_url):
+    url = urllib.request.urlopen(full_api_url)
+    output = url.read().decode('utf-8')
+    raw_api_dict = json.loads(output)
+    url.close()
+    return raw_api_dict
+
+
+def data_organizer(raw_api_dict):
+    data = dict(
+        city=raw_api_dict.get('name'),
+        country=raw_api_dict.get('sys').get('country'),
+        temp=raw_api_dict.get('main').get('temp'),
+        temp_max=raw_api_dict.get('main').get('temp_max'),
+        temp_min=raw_api_dict.get('main').get('temp_min'),
+        humidity=raw_api_dict.get('main').get('humidity'),
+        pressure=raw_api_dict.get('main').get('pressure'),
+        sky=raw_api_dict['weather'][0]['main'],
+        sunrise=time_converter(raw_api_dict.get('sys').get('sunrise')),
+        sunset=time_converter(raw_api_dict.get('sys').get('sunset')),
+        wind=raw_api_dict.get('wind').get('speed'),
+        wind_deg=raw_api_dict.get('deg'),
+        dt=time_converter(raw_api_dict.get('dt')),
+        cloudiness=raw_api_dict.get('clouds').get('all')
+    )
+
+
+    return data
+
+
+def data_output(data):
+    m_symbol = '\xb0' + 'C'
+    print('---------------------------------------')
+    print('Current weather in: {}, {}:'.format(data['city'], data['country']))
+    print(data['temp'], m_symbol, data['sky'])
+    print('Max: {}, Min: {}'.format(data['temp_max'], data['temp_min']))
+    print('')
+    print('Wind Speed: {}, Degree: {}'.format(data['wind'], data['wind_deg']))
+    print('Humidity: {}'.format(data['humidity']))
+    print('Cloud: {}'.format(data['cloudiness']))
+    print('Pressure: {}'.format(data['pressure']))
+    print('Sunrise at: {}'.format(data['sunrise']))
+    print('Sunset at: {}'.format(data['sunset']))
+    print('')
+    print('Last update from the server: {}'.format(data['dt']))
+    print('---------------------------------------')
+
+
+    
+    
+
+
+if __name__ == '__main__':
+    try:
+        data_output(data_organizer(data_fetch(url_builder(2172797))))
+    except IOError:
+        print('no internet')
